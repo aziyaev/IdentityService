c22e8319a1613684c359962372c39516b3eb6547
aziyaev <aziyaev99@gmail.com>
2023-05-10 19:10:53 +0300

    bug fixed

---
+++ Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using IdentityService.Models;
+
+namespace IdentityService.Interfaces
+{
+    public interface IUserService
+    {
+        User? Authenticate(string email, string password);
+        User? GetById(int id);
+    }
+}
